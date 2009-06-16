228fafeefe5d7e3239087ac28804c2144148ff4b
Tim Herd <erauqssidlroweht@eqdw.net>
2009-06-16 18:37:42 -0500

    This is really ugly. Fix admin_controller#index asap!

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -25,7 +25,23 @@ class AdminController < ApplicationController
   def index
     @bugs = nil
     if request.post?
-      @bugs = Bug.find_all_by_user_id(User.find_by_name(params[:username]))
+      @bugs = Bug.find(:all)
+      bugsbycompany = \
+        (params[:user][:company_name].blank? ? nil : Bug.find_all_by_user_id( \
+                       User.find_by_company_name(params[:user][:company_name])))
+      
+      bugsbyname = \
+        (params[:user][:name].blank? ? nil : Bug.find_all_by_user_id( \
+                    User.find_by_name(params[:user][:name])))
+      
+      bugsbysubmitter = \
+        (params[:bug][:submitter].blank? ? nil : Bug.find_all_by_submitter( \
+                                                    params[:bug][:submitter]))
+
+      @bugs.delete_if {|x| !bugsbycompany.include?(x)} if bugsbycompany
+      @bugs.delete_if {|x| !bugsbyname.include?(x)} if bugsbyname
+      @bugs.delete_if {|x| !bugsbysubmitter.include?(x)} if bugsbysubmitter
+      
     end
   end
 
